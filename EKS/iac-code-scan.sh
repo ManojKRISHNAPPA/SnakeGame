72c11ffc22f87c99f13436560a147998df279784
Manoj-K <contactmanojmech@gmail.com>
2026-03-18 20:44:27 +0530

    test

--- EKS/iac-code-scan.sh
+++ EKS/iac-code-scan.sh
@@ -3,6 +3,11 @@ set -e
 
 echo "Running Snyk IaC Scan..."
 
+SNYK_TOKEN="ca119e6e-bfe8-4c39-a29c-38575377963c"
+
+snyk auth $SNYK_TOKEN
+
+
 if snyk iac test --severity-threshold=critical; then
     echo "✅ No critical vulnerabilities found"
 else
