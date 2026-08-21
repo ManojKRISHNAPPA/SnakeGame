c453fa33e253e3df27e5945b4b0b384b030dbf8c
ManojKRISHNAPPA <contactmanojmech@gmail.com>
2026-08-21 20:06:24 +0530

    updated the type

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -96,7 +96,7 @@ resource "aws_eks_node_group" "itkannadigaru" {
     min_size     = 3
   }
 
-  instance_types = ["c7i-flex.large"]
+  instance_types = ["t2.medium"]
 
   remote_access {
     ec2_ssh_key               = var.ssh_key_name
